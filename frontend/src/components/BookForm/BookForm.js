0e4d6456fd22a8265558b02847de95ee52a3806b
Egor Gukov <108114245+when-dev@users.noreply.github.com>
2025-04-13 17:41:37 +0300

    Initialize Redux, add and display books

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,15 +1,19 @@
 import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addBook } from '../../redux/books/actionCreators'
 import './BookForm.css'
 
 const BookForm = () => {
 	const [title, setTitle] = useState('')
 	const [author, setAuthor] = useState('')
+	const dispatch = useDispatch()
 
 	const handleSubmit = e => {
 		e.preventDefault()
 
 		if (title && author) {
-			// dispatch action
+			dispatch(addBook({ title, author })) // dispatch action
+
 			setTitle('')
 			setAuthor('')
 		}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom/client';
+import store from './redux/store' 
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
 

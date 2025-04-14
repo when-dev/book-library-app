8150e62b4086336b4a7454e3d06c2d38eb2f58fb
Egor Gukov <108114245+when-dev@users.noreply.github.com>
2025-04-14 19:37:03 +0300

    Filter Redux slice and filter by title

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,12 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators'
 import { BsBookmarkStarFill, BsBookmarkStar } from 'react-icons/bs'
+import { selectTitleFilter } from '../../redux/slices/filterSlice'
 import './BookList.css'
 
 const BookList = () => {
 	const books = useSelector(state => state.books)
+	const titleFilter = useSelector(selectTitleFilter)
 	const dispatch = useDispatch()
-	console.log(books)
 
 	const handleDeleteBook = id => {
 		dispatch(deleteBook(id))
@@ -16,6 +17,13 @@ const BookList = () => {
 		dispatch(toggleFavorite(id))
 	}
 
+	const filteredBooks = books.filter(book => {
+		const matchesTitle = book.title
+			.toLowerCase()
+			.includes(titleFilter.toLowerCase())
+		return matchesTitle
+	})
+
 	return (
 		<div className='app-block book-list'>
 			<h2>Book List</h2>
@@ -23,7 +31,7 @@ const BookList = () => {
 				<p>No books available</p>
 			) : (
 				<ul>
-					{books.map((book, i) => (
+					{filteredBooks.map((book, i) => (
 						<li key={book.id}>
 							<div className='book-info'>
 								{++i}. {book.title} by <strong>{book.author}</strong>

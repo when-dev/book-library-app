bcdd80665f63aa3e6b8a83b9e86fe746417951cb
Egor Gukov <108114245+when-dev@users.noreply.github.com>
2025-04-15 19:43:10 +0300

    Added isLoading and spinner in the BookForm

--- api/index.js
+++ api/index.js
@@ -1,18 +1,28 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 const bookData = require('./data/books.json')
 
 const app = express()
 
 app.use(cors())
 
-app.get('/random-book', (req, res) => {
+function getRandomBook() {
 	const randomIndex = Math.floor(Math.random() * bookData.length)
 	const randomBook = bookData[randomIndex]
-	res.json(randomBook)
+	return randomBook
+}
+
+app.get('/random-book', (req, res) => {
+	res.json(getRandomBook())
+})
+
+app.get('/random-book-delayed', (req, res) => {
+	setTimeout(() => {
+		res.json(getRandomBook())
+	}, 2000)
 })
 
 const port = process.env.PORT || 4000
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`)
 })
--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
+import { FaSpinner } from 'react-icons/fa'
 import { addBook, fetchBook } from '../../redux/slices/booksSlice'
 import { setError } from '../../redux/slices/errorSlice'
 import booksData from '../../data/books.json'
@@ -9,6 +10,7 @@ import './BookForm.css'
 const BookForm = () => {
 	const [title, setTitle] = useState('')
 	const [author, setAuthor] = useState('')
+	const [isLoading, setIsLoading] = useState(false)
 	const dispatch = useDispatch()
 
 	const handleRandomBook = () => {
@@ -31,8 +33,13 @@ const BookForm = () => {
 		}
 	}
 
-	const handleAddRandomBookViaAPI = () => {
-		dispatch(fetchBook('http://localhost:4000/random-book'))
+	const handleAddRandomBookViaAPI = async() => {
+		try {
+			setIsLoading(true)
+			await dispatch(fetchBook('http://localhost:4000/random-book-delayed'))
+		} finally {
+			setIsLoading(false)
+		}
 	}
 
 	return (
@@ -61,8 +68,20 @@ const BookForm = () => {
 				<button type='button' onClick={handleRandomBook}>
 					Add Random
 				</button>
-				<button type='button' onClick={handleAddRandomBookViaAPI}>
-					Add Random via API
+
+				<button
+					type='button'
+					onClick={handleAddRandomBookViaAPI}
+					disabled={isLoading}
+				>
+					{isLoading ? (
+						<>
+							<span>Loading Book...</span>
+							<FaSpinner className='spinner' />
+						</>
+					) : (
+						'Add Random via API'
+					)}
 				</button>
 			</form>
 		</div>
